1f9d65c18b213957ad1be3b4f07c1723ce2db12b
Helena Avis <helenaavis48@gmail.com>
2025-03-07 14:46:26 +0000

    Added playlist endpoint to return example JSON data for a single playlist.

--- server.js
+++ server.js
@@ -10,8 +10,16 @@ app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
 
+// GET /api/playlists/
 app.get('/api/playlists/', (req, res) => {
     // return example JSON data - remove once database query is implemented
     const json = require('./exampleData/playlists.json');
+    res.status(200).send(json);
+});
+
+// GET /api/playlists/:playlistID
+app.get('/api/playlists/:playlistID', (req, res) => {
+    // access the playlistID from the URL by using req.params.playlistID
+    const json = require('./exampleData/playlistSingle.json');
     res.status(200).send(json);
 });

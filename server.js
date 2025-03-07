278244ef0001db65312efa914cdda66fb98d3bd5
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2025-03-07 15:58:27 +0000

    Added npm pg to project
    Added pg to the project.
    Added dotenv to the project.
    Set up .env file.
    Added some basic code for connecting to the Supabase database, but, unfortunately it has a timed out error.
    As a fix, have set up another database with Xata - this connection works.

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import pg from 'pg';
 
+dotenv.config();
 
-const express = require('express');
 const app = express();
 const port = 3000;
+const connectionString = process.env.XA_DB_CSTRING;
+
+const { Client } = pg;
+const client = new Client({
+    connectionString
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
+
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 });
+
+console.log("Starting connection");
+await client.connect();
+client.on('error', (err) => {
+    console.error('An error has accured!', err.stack);
+});
+
+const results = await client.query('SELECT * FROM playlists');
+
+console.log("results: ", results);
+
+await client.end();

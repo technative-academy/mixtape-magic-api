27d6d962d786f5344b540cdead7ec12c04edc435
Helena Avis <helenaavis48@gmail.com>
2025-03-10 16:05:14 +0000

    Implemented (parameterised) SQL queries for the playlists endpoint.

--- src/routes/playlists.js
+++ src/routes/playlists.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import pool from '../db.js';
 import playlistsJson from '../../exampleData/playlists.json' with { type: 'json' };
 import playlistsSingleJson from '../../exampleData/playlistSingle.json' with { type: 'json' };
 
@@ -7,15 +8,31 @@ const router = express.Router();
 // GET /api/playlists/
 router.get('/', async (req, res) => {
     // return example JSON data - remove once database query is implemented
-    const json = playlistsJson;
-    res.status(200).json(json);
+    // const json = playlistsJson;
+    // res.status(200).json(json);
+
+    try {
+        const results = await pool.query('SELECT * FROM playlists');
+        res.json(results.rows);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 // GET /api/playlists/:playlistID
-router.get('/:playlistID', (req, res) => {
+router.get('/:playlistID', async (req, res) => {
     // access the playlistID from the URL by using req.params.playlistID
-    const json = playlistsSingleJson;
-    res.status(200).json(json);
+    // const json = playlistsSingleJson;
+    // res.status(200).json(json);
+
+    // construct parameterized query
+    const query = 'SELECT * FROM playlists WHERE id = ?';
+    try {
+        const results = await pool.query(query, [req.params.playlistID]);
+        res.json(results.rows);
+    } catch {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 export default router;

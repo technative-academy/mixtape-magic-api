1a717806d233f141ae95341b71037b1e9b4be40f
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2025-03-10 15:44:43 +0000

    Added port setting to pg pool

--- src/db.js
+++ src/db.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 
 const { Pool } = pg;
 const connectionString = process.env.XA_DB_CSTRING;
+const port = process.env.PORT;
 
 dotenv.config();
 
 const pool = new Pool({
     connectionString,
+    port,
 });
 
 export default pool;

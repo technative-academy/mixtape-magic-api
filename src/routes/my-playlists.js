78a9842ea3f655da4b00f82c07df1cc75db3bc99
Helena Avis <helenaavis48@gmail.com>
2025-03-10 13:01:48 +0000

    Added routing stubs to my-playlists for other HTTP methods.

--- src/routes/my-playlists.js
+++ src/routes/my-playlists.js
@@ -16,4 +16,22 @@ router.get('/api/my-playlists/:playlistID', (req, res) => {
     res.status(200).json(json);
 });
 
+// POST /api/my-playlists/
+router.post('/', (req, res) => {
+    // create a new playlist for the logged in user
+    res.status(201);
+});
+
+// PATCH /api/my-playlists/:playlistID
+router.patch('/api/my-playlists/:playlistID', (req, res) => {
+    // update the specified playlist
+    res.status(200);
+});
+
+// DELETE /api/my-playlists/:playlistID
+router.delete('/api/my-playlists/:playlistID', (req, res) => {
+    // delete the specified playlist
+    res.status(204);
+});
+
 export default router;

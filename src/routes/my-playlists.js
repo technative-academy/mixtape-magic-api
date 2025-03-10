7f1cfa06722ed830bb8fab2816df3e12c4404316
Helena Avis <helenaavis48@gmail.com>
2025-03-10 12:52:35 +0000

    Added route file for my-playlists endpoint and added it to the main routes file.

--- src/routes/my-playlists.js
+++ src/routes/my-playlists.js
@@ -2,17 +2,17 @@ import express from 'express';
 
 const router = express.Router();
 
-// GET /api/playlists/
+// GET /api/my-playlists/
 router.get('/', async (req, res) => {
     // return example JSON data - remove once database query is implemented
-    const json = require('../../exampleData/playlists.json');
+    const json = require('../../exampleData/myplaylists.json');
     res.status(200).json(json);
 });
 
-// GET /api/playlists/:playlistID
-router.get('/api/playlists/:playlistID', (req, res) => {
+// GET /api/my-playlists/:playlistID
+router.get('/api/my-playlists/:playlistID', (req, res) => {
     // access the playlistID from the URL by using req.params.playlistID
-    const json = require('../../exampleData/playlistSingle.json');
+    const json = require('../../exampleData/myplaylistsSingle.json');
     res.status(200).json(json);
 });
 

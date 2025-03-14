a1c1a8abae18351d60f705cdba000961599f2874
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2025-03-14 12:49:08 +0000

    Added database query for user page
    The /me endpoint should now return the user details based on user id passed to the API from the front end.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -13,7 +13,7 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
         const { id, name, accessToken, refreshToken } = await loginUser(email, password);
--- src/routes/me.js
+++ src/routes/me.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import pool from '../db.js';
+import authenticateToken from '../middlewere/auth.js';
 import meJson from '../../exampleData/me.json' with { type: 'json' };
 
 const router = express.Router();
 
 // Get /api/me/
-router.get('/', async, (req, res) => {
-    const json = meJson;
-    res.status(200).json(json);
+router.get('/me', authenticateToken, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(400).json({ error: 'User not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 router.put('/', async, (req, res) => {});

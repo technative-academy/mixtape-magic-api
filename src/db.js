1aa7bc70bd51ef626077ddb06d60482c183e1a22
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2025-03-14 14:34:36 +0000

    Fixed semantic error in db.js
    The config was being loaded after environmental variables were being requested from the .env file, causing an error in connection when running a local instance. Moved .env file loading to top of code.

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const { Pool } = pg;
 const connectionString = process.env.XA_DB_CSTRING;
 const port = process.env.PORT;
-
-dotenv.config();
 
 const pool = new Pool({
     connectionString,
